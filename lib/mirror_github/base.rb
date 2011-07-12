2bb96481e395b0127eb64efbbf719f5eaee120a4
Casey Dreier <cdreier@gmail.com>
2011-07-12 16:21:11 -0400

    Allowing command-line arguments to set specific values.

--- lib/mirror_github/base.rb
+++ lib/mirror_github/base.rb
@@ -2,6 +2,10 @@ module MirrorGithub
 
   class Base
 
+    def initialize(args = nil)
+      parse_arguments(args)
+    end
+
     # Backs up all repositories from the organization defined in the config.
     def mirror_all
       puts "Mirroring #{github_connection.repositories.size} repositories..."
@@ -27,6 +31,17 @@ module MirrorGithub
 
     def github_connection
       @github_connection ||= Github.new(config.username, config.password, config.org)
+    end
+
+    def parse_arguments(args)
+      args.each do |arg|
+        option, value = arg.split('=').map { |v| v.chomp.strip }
+        next unless option && value
+        case option
+          when "--config-file" then Configuration.config_file_path = value
+          when "--backup-dir"  then config.backup_directory = value
+        end
+      end
     end
 
   end
